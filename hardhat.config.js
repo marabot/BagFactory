d4dd68b6b1f4cbf5a875125c6412e0398ef6c7cd
A.Charcosset <antoine.charcosset@sfr.fr>
2024-03-10 19:45:00 +0100

    add hardhat fork config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-truffle5");
+require('dotenv').config();
 
 module.exports = {
   solidity: {
@@ -10,7 +11,14 @@ module.exports = {
   },  
   sources: "client/src/contracts",
   networks: {
-    hardhat :{},
+    hardhat :{
+        forking: {
+        enabled: true,        
+        url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+        blockNumber: 19406433
+        
+      },
+    },
     sepolia: {
         url : "https://ethereum-sepolia-rpc.publicnode.com",
         accounts: "remote"
