5cd20b1193f38db8bb05bb074c670ac9d8b343d5
A.Charcosset <antoine.charcosset@sfr.fr>
2024-03-13 02:39:17 +0100

    clean 3 unused const

--- contracts/Bag.sol
+++ contracts/Bag.sol
@@ -16,10 +16,6 @@ contract Bag{
         // Tips by owner
         mapping(address => VaultStruct.Tip) Tips;
         ISupraSValueFeed internal sValueFeed;
-
-        address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-        address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-        address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
         
         // make router inherit instead of a constructor parameter
         address public swapRouter;
